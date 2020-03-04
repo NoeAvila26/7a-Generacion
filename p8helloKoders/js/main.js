c2dbb845de2ad11f747c6c8f45f88ad1253c1a1e
noe lopez avila <noerr_@hotmail.com>
2020-03-04 17:59:41 -0600

    2020-03-04 practica_1 javascript

---
+++ p8helloKoders/js/main.js
@@ -0,0 +1,11 @@
+
+var x = 6;
+var y = 4;
+console.log("la suma de x+y=somenumber",x+=y)
+console.log("la resta de x-y=somenumber",x-=y)
+console.log("la multiplicacion de x*y=somenumber",x*=y)
+console.log("la division de x/y=somenumber",x/=y)
+
+
+
+
