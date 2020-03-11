8104a877040064ce8813ea01eba3b6322f0a8eec
noe lopez avila <noerr_@hotmail.com>
2020-03-11 16:51:00 -0600

    practica objetos js

---
+++ p14objects/js/main.js
@@ -0,0 +1,47 @@
+// //crear una funcion que le pida al usuario las propiedades  nombre, apellido, edad
+// y el telefono de un koder y crear un objeto {koderObject} con esos datos
+
+//pedirle al usurio mediante un prompt nombre, apellidos, edad, telefono
+
+
+
+//metodo 1
+
+createdKoderObject = (name,lastName,age,telephoneNumber) => {
+    let koderObject = {name,lastName,age,telephoneNumber}
+    console.log(koderObject)
+    
+  
+}
+
+
+//metodo2
+
+createdKoderObeject = () => {
+  let name = prompt("escribe aqui tu nombre") 
+  let lastName = prompt("escribe tu apellido")
+  let age = prompt("escribe tu edad")
+  let telephoneNumber = prompt("escribe tu numero telefonico")
+  let koderObject = {name, lastName, age, telephoneNumber}
+  console.log(koderObject)
+
+}
+createdKoderObeject()
+
+//metodo3
+
+const createdKoderObeject = () => {
+let koderObeject{
+  name:",
+  lastName:",
+  age:",
+  telephoneNumber:"
+};
+for (prop in koderObeject) {
+  let pro = prompt(`ingrese el ${prop} del objeto`)
+  koderObeject[prop] = pro;
+
+}
+console.log(koderObeject);
+
+}
